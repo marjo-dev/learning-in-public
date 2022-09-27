383608b0b3cb163ea19b40ff1564dfc6f6cda0a7
Jonah M <jmarian.dev@gmail.com>
2022-09-26 22:01:06 -0400

    added new files to document future codewars solutions

---
+++ codewars/app.js
@@ -0,0 +1,28 @@
+// codewars 9/26
+// Who likes it? (6 kyu: strings, fundamentals) 
+
+// Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+
+// []                                -->  "no one likes this"
+// ["Peter"]                         -->  "Peter likes this"
+// ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+// ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+// ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+// Note: For 4 or more names, the number in "and 2 others" simply increases.
+
+//I used a simple if... else statement for this. plan to refactor using switch statements or a spread operator later
+function likes(arr) {
+  if (arr.length === 0) {
+    return 'no one likes this';
+  } else if (arr.length === 1) {
+    return `${arr[0]} likes this`;
+  } else if (arr.length === 2) {
+    return `${arr[0]} and ${arr[1]} like this`;
+  } else if (arr.length === 3) {
+    return `${arr[0]}, ${arr[1]} and ${arr[2]} like this`;
+  } else if (arr.length > 3) {
+    return `${arr[0]}, ${arr[1]} and ${arr.length - 2} others like this`;
+  }
+}
+//testing:
+console.log(likes(['socks', 'mittens', 'chippy', 'seamus', 'perdy']));
