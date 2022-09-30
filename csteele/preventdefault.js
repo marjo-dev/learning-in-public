1ea46ed45525155898578e545d2ba779b6b96be1
Jonah M <jmarian.dev@gmail.com>
2022-09-29 23:27:48 -0400

    added form input & change event example

--- csteele/preventdefault.js
+++ csteele/preventdefault.js
@@ -1,3 +1,15 @@
+// //265. INPUT AND CHANGE EVENTS EXERCISE
+// The h2 should start with the text "Enter Your Username"
+// Whenever an input event is fired on the <input> element, update the <h2> so that it displays "Welcome, " plus the current value from the text input.  Take a look at the gif below to see how it should work.
+// If the <input> goes back to being empty, update the <h2> so that it once again says "Enter Your Username"
+const inputChange = document.querySelector('#username');
+const h2 = document.querySelector('#change');
+inputChange.addEventListener('input', function (e) {
+    if (inputChange.value === '') {
+        h2.innerText = 'Enter Your Username'
+    } else h2.innerText = 'Welcome, ' + inputChange.value;
+})
+
 // //262. FORM EVENTS AND PREVENT DEFAULT
 
 const petsForm = document.querySelector('#shelterForm');
