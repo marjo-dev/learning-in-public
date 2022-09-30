2c074bf66f6054d8f325b7fe4d0b2730f080486f
Jonah M <jmarian.dev@gmail.com>
2022-09-29 20:50:51 -0400

    added refactored code to codewars 9/26

--- codewars/app.js
+++ codewars/app.js
@@ -24,5 +24,17 @@ function likes(arr) {
     return `${arr[0]}, ${arr[1]} and ${arr.length - 2} others like this`;
   }
 }
+
+function likeThis(arr) {
+  switch (arr.length) {
+    case 0: return `no one likes this`; break;
+    case 1: return `${arr[0]} likes this`; break;
+    case 2: return `${arr[0]} and ${arr[1]} like this`; break;
+    case 3: return `${arr[0]}, ${arr[1]} and ${arr[2]} like this`; break;
+    default: return `${arr[0]}, ${arr[1]} and ${arr.length - 2} others like this`; break;
+  }
+}
+
 //testing:
 console.log(likes(['socks', 'mittens', 'chippy', 'seamus', 'perdy']));
+console.log(likeThis(['patches', 'ripley', 'junior']));
